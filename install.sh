9e3ac01148c79f58c3815804092220adeea38a3c
Lorcan Chinnock <ljchinnock@gmail.com>
2026-02-06 10:27:31 +0000

    Fix remote tmux install config error

--- install.sh
+++ install.sh
@@ -74,13 +74,22 @@ THEME="default"
 INSTALL_TPM="yes"
 SHELL_TYPE="auto"
 
-# Color codes for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-ORANGE='\033[0;33m'
-NC='\033[0m' # No Color
+# Color codes for output (TTY only)
+if [[ -t 1 ]]; then
+  RED=$'\033[0;31m'
+  GREEN=$'\033[0;32m'
+  YELLOW=$'\033[1;33m'
+  BLUE=$'\033[0;34m'
+  ORANGE=$'\033[0;33m'
+  NC=$'\033[0m' # No Color
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  ORANGE=""
+  NC=""
+fi
 
 # ========================================
 # Helper Functions
